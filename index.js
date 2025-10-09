99d64ef435967e8ebf687febfec1154d0a9218e7
Ulises <u.ivalero@hotmail.com>
2025-10-09 18:26:33 -0300

    v.07 Render failed deploy

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
 import cors from 'cors'
 import authRoutes from './routes/auth.routes.js'
 
-dotenv.config()
 const app = express()
 app.use(cors())
 // {
